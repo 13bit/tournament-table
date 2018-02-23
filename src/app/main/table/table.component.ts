217bb758ccd83531a6e7c250bffc95709a4ead28
remdi <dsr.workk@gmail.com>
2018-02-23 13:52:14 +0200

    - updated table component

--- src/app/main/table/table.component.ts
+++ src/app/main/table/table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../UserService';
+import {User} from '../../models/User';
 
 @Component({
   selector: 'app-table',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.getUsers();
+    setInterval(() => this.getUsers(), 15000);
   }
 
+  getUsers() {
+    this.userService.get()
+      .subscribe((users) => {
+        this.users = users.sort((a, b) => b.bonus - a.bonus);
+      });
+  }
+
+  scroll() {
+    setInterval(() => {
+      window.scrollBy({
+        top: 25,
+        behavior: 'smooth'
+      });
+    }, 1000);
+
+  }
 }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,6 +1,6 @@
 export class User {
   id: any;
   name: string;
-  bonus: number;
+  bonus: number = 0;
   bonusHistory: Array<number> = [];
 }
